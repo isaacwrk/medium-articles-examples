d4ec4056ab4fe4cd0f680d37d624f4686952c296
isaacwrk <isaacsouzawrk@gmail.com>
2022-02-13 10:34:30 -0300

    initial commit

---
+++ vue-skeleton-tailwind/src/components/Page.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-300 to-green-400">
+    <slot/>
+</div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const Page = defineComponent({
+});
+
+export default Page;
+</script>
+
+<style>
+
+</style>
