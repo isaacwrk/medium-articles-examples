dd822cbb4d1b9f0f0041ba36e38908c0911eece9
isaacwrk <isaacsouzawrk@gmail.com>
2022-02-13 11:04:08 -0300

    feature: Insert Card component and Skeleton component

---
+++ vue-skeleton-tailwind/src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+ <div class="w-72 bg-white rounded shadow-2xl">
+    <!-- image -->
+    <div class="h-48 bg-gray-200 rounded-tr rounded-tl">
+        <img src="~@/assets/images/tanjiro.jpg" class="h-48 w-full object-fill" alt="Tanjiro Kamado">
+    </div>
+
+    <div class="p-5">
+      <!-- title -->
+      <div class="h-6 rounded-sm mb-4">
+        <h1 class="font-semibold text-gray-600 text-lg">Tanjiro Kamado</h1>
+      </div>
+
+      <!-- content -->
+      <div class="text-left">
+        <div class="h-42 bg-gray-200 rounded-sm px-2">
+          <p class="break-words">A jornada de Tanjiro tem como fio principal 2 objetivos:</p>
+          <p>transformar sua irmã novamente em humana e se vingar do demônio que destruiu sua família.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const Card = defineComponent({
+});
+
+export default Card;
+</script>
+
+<style>
+
+</style>
--- vue-skeleton-tailwind/src/views/Home.vue
+++ vue-skeleton-tailwind/src/views/Home.vue
@@ -1,43 +1,21 @@
 <template>
-
   <Page>
-  <!-- card -->
-  <div class="w-64 bg-white rounded shadow-2xl">
-
-    <!-- image -->
-    <div class="h-32 bg-gray-200 rounded-tr rounded-tl animate-pulse"></div>
-
-    <div class="p-5">
-      <!-- title -->
-      <div class="h-6 rounded-sm bg-gray-200 animate-pulse mb-4"></div>
-
-      <!-- content -->
-      <div class="grid grid-cols-4 gap-1">
-        <div class="col-span-3 h-4 rounded-sm bg-gray-200 animate-pulse"></div>
-        <div class="h-4 rounded-sm bg-gray-200 animate-pulse"></div>
-
-        <div class="col-span-2 h-4 rounded-sm bg-gray-200 animate-pulse"></div>
-        <div class="col-span-2 h-4 rounded-sm bg-gray-200 animate-pulse"></div>
-
-        <div class="h-4 rounded-sm bg-gray-200 animate-pulse"></div>
-        <div class="col-span-3 h-4 rounded-sm bg-gray-200 animate-pulse"></div>
-        <div class="col-span-2 h-4 rounded-sm bg-gray-200 animate-pulse"></div>
-        <div class="h-4 rounded-sm bg-gray-200 animate-pulse"></div>
-      </div>
-    </div>
-  </div>
+    
   </Page>
-
 
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import Page from '@/components/Page.vue'
+import Card from '@/components/Card.vue'
+import Skeleton from "@/components/Skeleton.vue"
 
 @Options({
   components: {
-    Page
+    Page,
+    Card,
+    Skeleton
   },
 })
 export default class Home extends Vue {}
