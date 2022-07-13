18ca14cf8805cb3f26b38c82841448fc7c1cca1c
isaacwrk <isaacsouzawrk@gmail.com>
2022-07-13 18:00:45 -0300

    feature:add prefers-color-scheme dark

--- tailwind-prefers-color-scheme/src/App.vue
+++ tailwind-prefers-color-scheme/src/App.vue
@@ -1,11 +1,22 @@
+<template>
+
+<div class="min-h-screen flex items-center justify-center dark:bg-slate-700">
+  <div class="p-20 bg-orange-200 dark:bg-purple-200 mx-6">
+  <h3 class="text-orange-400 dark:text-purple-500 font-bold mb-4">Tailwind Dark / Light Theme!</h3>
+    <div class="bg-white dark:bg-gray-800 rounded-lg shadow-2xl md:flex">
+      <img src="https://images.unsplash.com/photo-1593642532744-d377ab507dc8" alt="Laptop on Desk" class="md:w-1/3 rounded-t-lg md:rounded-l-lg md:rounded-t-none" />
+      <div class="p-6">
+        <h2 class="font-bold text-xl md:text-3xl mb-2 text-orange-700 dark:text-purple-700">Horizontal Card</h2>
+        <p class="text-orange-700 dark:text-purple-700">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</template>
 <script setup lang="ts">
 </script>
-
-<template>
-<div class="bg-red-200 text-center">
-  opa
-</div>
-</template>
-
 <style>
 </style>
